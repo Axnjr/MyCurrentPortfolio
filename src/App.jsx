5eca7496a17514d1e4573b108d24667deb51fa11
Axnjr <yakshitchhipa@gmail.com>
2023-03-15 14:19:12 +0530

    updated resume link

--- src/App.jsx
+++ src/App.jsx
@@ -75,7 +75,7 @@ export default function Home() {
 						<a href='https://github.com/Axnjr'><p>Github</p></a>
 						<a href='https://www.linkedin.com/in/yakshit/'><p>LinkedIn</p></a>
 						<a href="mailto:yakshitchhipa@gmail.com"><p>Mail</p></a>
-						<a href='https://drive.google.com/file/d/1OqikU2PYGF0qyzejatS_bc2muo3CGtRG/view'><p>Resume</p></a>
+						<a href='https://drive.google.com/file/d/18LimNG29KNYusZjOvwUWEqCI-1SbG_n9/view?usp=sharing'><p>Resume</p></a>
 					</div>
 					<div className='hamburger-menu-button'>
 						<section style={{display:"block"}} onClick={MenuAppear}>
