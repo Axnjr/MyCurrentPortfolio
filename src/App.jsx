094b99a87c3e6c56d0aeef2e33f0401a30030b7f
Yakshit <yakshitchhipa@gmail.com>
2024-08-31 22:16:35 +0530

    update: resume link

--- src/App.jsx
+++ src/App.jsx
@@ -74,7 +74,7 @@ export default function Home() {
 						<a href='https://twitter.com/axnsscode'><p>Twitter</p></a>
 						<a href='https://www.linkedin.com/in/yakshit/'><p>LinkedIn</p></a>
 						<a href="mailto:yakshitchhipa@gmail.com"><p>Mail</p></a>
-						<a href='https://drive.google.com/file/d/1YojEAy2zMcEZj1VjyZwfDCojIlNhrwvt/view'><p>Resume</p></a>
+						<a href='https://drive.google.com/file/d/1z3V6LewdSbW3bnMreJVaJZ6G7hfTPi5d/view'><p>Resume</p></a>
 					</div>
 					<div className='hamburger-menu-button'>
 						<section style={{ display: "block" }} onClick={MenuAppear}>
